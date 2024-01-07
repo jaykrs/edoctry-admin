b5931bece18f2ef6e24b8e64831363b2744ca284
Jayant Kumar <jaykrs@gmail.com>
2024-01-07 21:08:12 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,17 +1,28 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'mysql');
+  const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
     mysql: {
       connection: {
-      host: env('DATABASE_HOST', 'bftoonzu9tfxfcpyfxbc-mysql.services.clever-cloud.com'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'bftoonzu9tfxfcpyfxbc'),
-      user: env('DATABASE_USERNAME', 'uqqu6lw0lc3pgjfq'),
-      password: env('DATABASE_PASSWORD', 'JHhvLdrin53EdythqaxL'),
-      ssl: env.bool('DATABASE_SSL', false)
+        connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false) && {
+        key: env('DATABASE_SSL_KEY', undefined),
+        cert: env('DATABASE_SSL_CERT', undefined),
+        ca: env('DATABASE_SSL_CA', undefined),
+        capath: env('DATABASE_SSL_CAPATH', undefined),
+        cipher: env('DATABASE_SSL_CIPHER', undefined),
+        rejectUnauthorized: env.bool(
+            'DATABASE_SSL_REJECT_UNAUTHORIZED',
+            true
+          ),
+        },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
