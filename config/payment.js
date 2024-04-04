3f1ff4b29ded326e2b8ab4737a6b11b8fa170121
Anand  Kumar Das <81643298+Anand370@users.noreply.github.com>
2024-04-04 23:38:00 +0530

    Update payment.js

--- config/payment.js
+++ config/payment.js
@@ -1,4 +1,4 @@
-module.exports = {
-    PAYMENT_KEY_ID : "rzp_test_j01bme0LQCu7jS",
-    PAYMENT_SECRECT : "Z3H4ETaMZwR7CYK6ppAoZKBf"
+exports.info = {
+    P_KEY_ID : "rzp_test_j01bme0LQCu7jS",
+    P_SECRECT : "Z3H4ETaMZwR7CYK6ppAoZKBf"
 };
