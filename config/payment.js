a224373e41057c370ae0842b3c52308f05281735
Anand  Kumar Das <81643298+Anand370@users.noreply.github.com>
2024-04-06 00:39:14 +0530

    config update

--- config/payment.js
+++ config/payment.js
@@ -1,4 +1,9 @@
 exports.info = {
     P_KEY_ID : "rzp_test_j01bme0LQCu7jS",
-    P_SECRECT : "Z3H4ETaMZwR7CYK6ppAoZKBf"
+    P_SECRECT : "Z3H4ETaMZwR7CYK6ppAoZKBf",
+    api_key: "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",
+    url: "https://edoctry.adaptable.app",
+    brevoUrl:"https://api.brevo.com/v3/smtp/email",
+    email: "noreply@edoctry.com",
+    name: "Noreply Support"
 };
