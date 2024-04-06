6bddcab10fe2477235e2b99e7bd45fb3f3623457
Anand  Kumar Das <81643298+Anand370@users.noreply.github.com>
2024-04-06 09:59:11 +0530

    Update payment.js

--- config/payment.js
+++ config/payment.js
@@ -1,4 +1,4 @@
-exports.info = {
+module.exports = {
     P_KEY_ID : "rzp_test_j01bme0LQCu7jS",
     P_SECRECT : "Z3H4ETaMZwR7CYK6ppAoZKBf",
     api_key: "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",
