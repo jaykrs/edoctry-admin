bc66047dae6a897fb632f0d991c5cb844d322ce4
anand kumar das <anand003das@gmail.com>
2024-04-09 18:58:40 +0530

    config updated

--- config/payment.js
+++ config/payment.js
@@ -1,7 +1,7 @@
 module.exports = {
     P_KEY_ID: "rzp_test_j01bme0LQCu7jS",
     P_SECRECT: "Z3H4ETaMZwR7CYK6ppAoZKBf",
-    api_key: "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",
+    api_key: "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",
     url: "https://edoctry.adaptable.app",
     brevoUrl: "https://api.brevo.com/v3/smtp/email",
     email: "noreply@edoctry.com",
