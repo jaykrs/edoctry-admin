c7b14dd1cc26bbebdfb6b3de41579dfc819c168c
Jayant Kumar <jaykrs@gmail.com>
2025-06-21 17:40:51 +0530

    db fix

--- config/database.js
+++ config/database.js
@@ -8,9 +8,9 @@ module.exports = ({ env }) => {
       connection: {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'edoctry-app'),
-        user: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'Welcome@0'),
+        database: env('DATABASE_NAME', ''),
+        user: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
         ssl: env.bool('DATABASE_SSL', false)
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 4) },
